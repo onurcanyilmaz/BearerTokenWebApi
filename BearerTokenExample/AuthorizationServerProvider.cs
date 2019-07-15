f531d20bb91ec632cd9914ad8a6dac4479c1a35d
onurcanyilmaz <oyilmaz1630@gmail.com>
2019-07-15 03:14:19 +0300

    initial commit

---
+++ BearerTokenExample/AuthorizationServerProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace BearerTokenExample
+{
+    public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            if (context.UserName.Equals("onurcan", StringComparison.OrdinalIgnoreCase) && context.Password == "yilmaz")
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim("name", context.UserName));
+                identity.AddClaim(new Claim("pass", context.Password));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Oturum Hatası", "Kullanıcı adı ve şifre hatalıdır");
+            }
+        }
+    }
+}
---
+++ BearerTokenExample/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+
+[assembly: OwinStartup(typeof(BearerTokenExample.Startup))]
+
+namespace BearerTokenExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration configuration = new HttpConfiguration();
+            Configure(app);
+            WebApiConfig.Register(configuration);
+            app.UseWebApi(configuration);
+        }
+
+        private void Configure(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions()
+            {
+                TokenEndpointPath = new Microsoft.Owin.PathString("/getToken"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                AllowInsecureHttp = true,
+                Provider = new AuthorizationServerProvider()
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
